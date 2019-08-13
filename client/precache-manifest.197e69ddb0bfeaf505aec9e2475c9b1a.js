3d2344c7a8bc226151d8845f10ab40e554b7019b
jimmyneutron8 <samcapehart@gmail.com>
2019-08-12 23:59:54 -0500

    Fixed bug with alias

---
+++ client/precache-manifest.197e69ddb0bfeaf505aec9e2475c9b1a.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dea79cc900292bc74dda717aa36acc4d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c6b42e1e2453e746bed9",
+    "url": "/static/css/main.b27b14ec.chunk.css"
+  },
+  {
+    "revision": "ff9a50739926fa1af327",
+    "url": "/static/js/2.d98cdc19.chunk.js"
+  },
+  {
+    "revision": "c6b42e1e2453e746bed9",
+    "url": "/static/js/main.2851c95f.chunk.js"
+  },
+  {
+    "revision": "d9d8691dd46a542409cf",
+    "url": "/static/js/runtime~main.431a1f64.js"
+  },
+  {
+    "revision": "02f11d1f91bda442b86a5763de986296",
+    "url": "/static/media/clear-day.02f11d1f.svg"
+  },
+  {
+    "revision": "3e4be6c9b810ffd6195ef467de082aec",
+    "url": "/static/media/clear-night.3e4be6c9.svg"
+  },
+  {
+    "revision": "5ef472e5f9d8dcb47e129944fd866ef0",
+    "url": "/static/media/cloudy.5ef472e5.svg"
+  },
+  {
+    "revision": "eee4f601c181ee6ac6ecaf91788cbef1",
+    "url": "/static/media/partly-cloudy-day.eee4f601.svg"
+  },
+  {
+    "revision": "96cc1826d593c4bb0ddd2e2037cb7abb",
+    "url": "/static/media/partly-cloudy-night.96cc1826.svg"
+  },
+  {
+    "revision": "1006b3287ce1eda20ad2212edb386a0b",
+    "url": "/static/media/rain.1006b328.svg"
+  },
+  {
+    "revision": "055defa044dc7d63297fe57275f1e980",
+    "url": "/static/media/sleet.055defa0.svg"
+  },
+  {
+    "revision": "e583d54562e964429b8027f91511c7d5",
+    "url": "/static/media/snow.e583d545.svg"
+  },
+  {
+    "revision": "78a95530cbd9f8d355e9c6914e4e55d0",
+    "url": "/static/media/wind.78a95530.svg"
+  }
+]);
